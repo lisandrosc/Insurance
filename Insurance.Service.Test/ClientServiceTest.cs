f0200571d6285baa3f2d49ee2260b0dcb41e5f39
Lisandro Schneir <lisandrosc@hotmail.com>
2018-06-12 10:09:52 -0300

    Add test project

---
+++ Insurance.Service.Test/ClientServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using NUnit.Framework;
+using Insurance.Mocky;
+using Rhino.Mocks;
+using System.Collections;
+using Insurance.Entities.Contracts;
+using System.Collections.Generic;
+
+namespace Insurance.Service.Test
+{
+    [TestFixture]
+    public class ClientServiceTest
+    {
+        #region Members
+
+        private IMockyService _mockyService;
+        private ClientService _target;
+
+        #endregion
+
+        #region Methods
+        [SetUp]
+        public void Setup()
+        {
+            _mockyService = MockRepository.GenerateMock<IMockyService>();
+            _target = new ClientService(_mockyService);
+        }
+
+        [Test]
+        public void Should_get_all_client_list()
+        {
+            // Act.-
+            _target.GetClientList();
+
+            // Assert.-
+            _mockyService.AssertWasCalled(e => e.GetClients(), opt => opt.Repeat.Times(1));
+        }
+        [Test]
+        public void Should_get_client_by_email()
+        {
+            // Arrange         
+            Client client = new Client { Id = "2", Email = "2@test.com" , Name = "Mike" };
+            _mockyService.Expect(e => e.GetClients()).Return(GetFakeClientList());
+
+            // Act.-
+            Client val = _target.GetClientByEmail(client.Email);
+
+            // Assert.-
+            Assert.IsTrue(val.Id.Equals(client.Id) && val.Email.Equals(client.Email));
+        }
+
+        [Test]
+        public void Should_get_client_by_name()
+        {
+            // Arrange         
+            Client client = new Client { Id = "3", Email = "3@test.com" ,Name = "Jane"};
+            _mockyService.Expect(e => e.GetClients()).Return(GetFakeClientList());
+
+            // Act.-
+            Client val = _target.GetClientByName(client.Name);
+
+            // Assert.-
+            Assert.IsTrue(val.Id.Equals(client.Id) && val.Email.Equals(client.Email));
+        }
+
+        private IEnumerable<Client> GetFakeClientList()
+        {
+            List<Client> list = new List<Client>();
+            list.Add(new Client { Id = "1", Email = "1@test.com" , Name = "Tomas" });
+            list.Add(new Client { Id = "2", Email = "2@test.com" , Name = "Mike" });
+            list.Add(new Client { Id = "3", Email = "3@test.com" , Name = "Jane" });
+            return list;
+        }
+        #endregion
+    }
+}
---
+++ Insurance.Service.Test/PolicyServiceTest.cs
@@ -0,0 +1,91 @@
+﻿using Insurance.Entities.Contracts;
+using Insurance.Mocky;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance.Service.Test
+{
+    public class PolicyServiceTest
+    {
+        [TestFixture]
+        public class ClientServiceTest
+        {
+            #region Members
+
+            private IMockyService _mockyService;
+            private IClientService _clientService;
+            private PolicyService _target;
+
+            #endregion
+
+            #region Methods
+            [SetUp]
+            public void Setup()
+            {
+                _mockyService = MockRepository.GenerateMock<IMockyService>();
+                _clientService = MockRepository.GenerateMock<IClientService>();
+                _target = new PolicyService(_clientService, _mockyService);
+            }
+
+            [Test]
+            public void Should_get_policies_by_user_name()
+            {
+                // Arrange      
+                Client client = new Client { Id = "2", Email = "2@test.com", Name = "Mike" };
+                _clientService.Expect(e => e.GetClientByName(Arg<string>.Is.Anything)).Return(client);
+                _mockyService.Expect(e => e.GetPolicies()).Return(GetFakePolicies());
+
+                // Act.-                
+                IEnumerable<Policy> val = _target.GetByUserName("Mike");                
+
+                // Assert.-                
+                Assert.IsTrue(val.Count() == 2);                
+            }
+
+            [Test]
+            public void Should_get_policies_by_clientId()
+            {
+                // Arrange      
+                Client client = new Client { Id = "3", Email = "3@test.com", Name = "Jane" };                
+                _mockyService.Expect(e => e.GetPolicies()).Return(GetFakePolicies());
+
+                // Act.-                
+                IEnumerable<Policy> val = _target.GetPoliciesByClientId("3");
+
+                // Assert.-                
+                Assert.IsTrue(val.Count() == 1);
+            }
+
+            [Test]
+            public void Should_get_user_by_policy_number()
+            {
+                // Arrange      
+                Client client = new Client { Id = "3", Email = "3@test.com", Name = "Jane" };
+                _mockyService.Expect(e => e.GetPolicies()).Return(GetFakePolicies());
+                _clientService.Expect(e => e.GetClient(Arg<string>.Is.Anything)).Return(client);
+
+                // Act.-                
+                Client val = _target.GetUserByPolicyNumber("f");
+
+                // Assert.-                
+                Assert.IsTrue(val.Id.Equals(client.Id));
+            }
+
+            private IEnumerable<Policy> GetFakePolicies()
+            {
+                List<Policy> list = new List<Policy>();
+                list.Add(new Policy { Id = "a", ClientId = "1", AmountInsured = "125.35" });                
+                list.Add(new Policy { Id = "c", ClientId = "2", AmountInsured = "105.39" });
+                list.Add(new Policy { Id = "d", ClientId = "2", AmountInsured = "356.02" });
+                list.Add(new Policy { Id = "f", ClientId = "3", AmountInsured = "189.78" });
+                return list;
+            }
+            #endregion
+        }
+    }
+}
